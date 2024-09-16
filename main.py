3338f69d9a9baf0bbd8a57c9020258123597db76
Rashnotech <rashnotech@gmail.com>
2024-09-16 14:29:05 +0000

    Remove few dependency from  workflow

--- main.py
+++ main.py
@@ -14,21 +14,23 @@ app.config.from_object(Config)
 CORS(app, resources={})
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(self):
     """Tear down econtext"""
     storage.close()
+
 
 @app.errorhandler(404)
 def notfound(error) -> dict:
     """Not found error"""
     return jsonify({'error': 'Not Found!'})
 
+
 @app.errorhandler(403)
 def forbidden(error) -> dict:
     """Forbidden error"""
     return jsonify({'error': 'Forbidden'})
-
 
 
 if __name__ == '__main__':
--- models/base.py
+++ models/base.py
@@ -17,19 +17,19 @@ class BaseModel:
         kwargs: key word arguments
     """
     id = Column(String(100), unique=True, primary_key=True,
-                default= lambda: str(uuid4), nullable=False)
+                default=lambda: str(uuid4), nullable=False)
     created_at = Column(DateTime, default=datetime.now(timezone.utc),
-            onupdate=datetime.now(timezone.utc), nullable=False)
+                        onupdate=datetime.now(timezone.utc), nullable=False)
     updated_at = Column(DateTime, default=datetime.now(timezone.utc),
-            nullable=False, onupdate=datetime.now(timezone.utc))
+                        nullable=False, onupdate=datetime.now(timezone.utc))
 
     def __init__(self, **kwargs) -> None:
         """initialization method"""
         if kwargs:
             for attr, val in kwargs.items():
                 if attr in ['created_at', 'updated_at']:
-                    setattr(self, attr, datetime.strptime(val,
-                                        '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(self, attr,
+                            datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f'))
                 elif attr == '__class__':
                     del attr
                 else:
@@ -39,7 +39,6 @@ class BaseModel:
             self.created_at = datetime.now(timezone.utc)
             self.updated_at = datetime.now(timezone.utc)
 
-    
     def __str__(self) -> str:
         """return a representation of the base model"""
         return '[{}] ({}) {}'.format(self.__class__.__name__,
@@ -54,7 +53,6 @@ class BaseModel:
         if '_sa_instance_state' in new_dict:
             del new_dict['_sa_instance_state']
         return new_dict
-
 
     def save(self) -> None:
         """update public instance attribute updated at"""
